cb72e536540d437a7ab259a2d1db794ad0f83747
Gabriel Aureo <gabrielaureo@gmail.com>
2017-07-05 19:39:53 -0300

    Line Renderer implemented

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour {
+
+	LineRenderer line;
+
+	// Use this for initialization
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer>();
+		SetupLine();
+		Swipe.onClick += setLineStart;
+		Swipe.onHold += moveLineEnd;
+		Swipe.onLift += endLine;
+	}
+
+	void Update(){
+		
+	}
+
+	void SetupLine(){
+		line.positionCount = 2;
+		line.SetPosition(0,Vector3.zero);
+		line.SetPosition(1,Vector3.zero);
+		line.startWidth = .5f;
+		line.endWidth = 0f;
+		line.numCapVertices = 0;
+	}
+	
+	void setLineStart(Vector2 start){
+		line.SetPosition(0,start);
+		line.SetPosition(1,start);
+		line.numCapVertices = 1;
+	}
+
+	void moveLineEnd(Vector2 end){
+		line.SetPosition(1,end);
+	}
+
+	void endLine(Vector2 end){
+		line.SetPosition(0,Vector3.zero);
+		line.SetPosition(1,Vector3.zero);
+		line.numCapVertices = 0;
+	}
+}
---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe: MonoBehaviour {
+	Vector2 clickPos;
+	Vector2 currentPos;
+	[SerializeField]
+	float angle;
+
+	public delegate void mouseClickHandler(Vector2 pos);
+	public static event mouseClickHandler onClick;
+
+	public delegate void mouseHoldHandler(Vector2 pos);
+	public static event mouseHoldHandler onHold;
+
+	public delegate void mouseLiftHandler(Vector2 pos);
+	public static event mouseLiftHandler onLift;
+
+	
+	void Update(){
+		if(Input.GetMouseButtonDown(0)){
+			clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log(clickPos);
+			if(onClick!=null){
+				onClick(clickPos);
+			}
+		}
+		if(Input.GetMouseButton(0)){
+			currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if(onHold!=null){
+				onHold(currentPos);
+			}
+		}
+
+		if(Input.GetMouseButtonUp(0)){
+			Vector2 v = currentPos - clickPos;
+			angle = Mathf.Atan2(1 - v.x, v.y) * Mathf.Rad2Deg;
+			if(onLift != null){
+				onLift(v);
+			}
+			
+		}
+
+
+	}
+
+	
+}
